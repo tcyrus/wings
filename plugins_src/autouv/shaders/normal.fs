71c22a085bab77f4a3338c43191f81ddba63bd0e
Dan Gudmundsson <dgud@users.sourceforge.net>
2015-12-10 22:39:10 +0100

    Make a 2 step normal shader

--- plugins_src/autouv/shaders/normal.fs
+++ plugins_src/autouv/shaders/normal.fs
@@ -10,12 +10,32 @@
 //
 
 varying vec3 w3d_normal;
+varying vec4 w3d_tangent;
+uniform float w3d_tangentspace;
+varying vec2 w3d_uv;
+uniform sampler2D normalmap;
 
 void main(void)
 {
-   vec3 color; 
-   // Scale normals range from: -1,1 to 0,1
-   color = (normalize(w3d_normal)+1.0)*0.5;
+   vec3 color;
+   if(w3d_tangentspace < 0.1) {
+       // Scale normals range from: -1,1 to 0,1
+       color = (normalize(w3d_normal)+1.0)*0.5;
+       gl_FragColor = vec4(color, 1.0);
+       return;
+   }
+   vec3 T = normalize(w3d_tangent.xyz);
+   vec3 N = normalize(w3d_normal.xyz);
+   T = normalize(T - dot(T, N) * N);
+   vec3 B = cross(T, N) * w3d_tangent.w;
+   mat3 InvTBN = transpose(mat3(T, B, N));
+
+   vec3 HighResNormal = texture2D(normalmap, w3d_uv.xy).xyz;
+   vec3 NewNormal = 2.0 * HighResNormal - vec3(1.0, 1.0, 1.0);
+   NewNormal = InvTBN * NewNormal;
+   color = normalize(NewNormal);
+   //color =  w3d_tangent.xyz-w3d_normal.xyz;
+   color = (color+1.0)*0.5;
    gl_FragColor = vec4(color, 1.0);
 }
 
--- plugins_src/autouv/shaders/normal.vs
+++ plugins_src/autouv/shaders/normal.vs
@@ -10,13 +10,18 @@
 //
  
 varying vec3 w3d_normal;
-
 uniform vec2 auv_texsz;
+
+varying vec2 w3d_uv;
+varying vec4 w3d_tangent;
 
 void main(void)
 {
-    w3d_normal = gl_Normal.xyz;
+    w3d_uv    = gl_Vertex.xy;
 
+    w3d_normal  = gl_Normal.xyz;
+    w3d_tangent = gl_MultiTexCoord2;
+    
     vec4 Position = gl_Vertex;
     gl_Position   = (gl_ModelViewProjectionMatrix * Position);
 }
